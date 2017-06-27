d008fbe269bdd8ef9eb9958b0b50a5f277dcd47b
nonameable <ddelgado2013@gmail.com>
2017-06-27 01:35:06 -0500

    added code for vtk calculator exploration

---
+++ lab5/scripts/06_NB_VolumeRendering_CalExploration.py
@@ -0,0 +1,101 @@
+import vtk
+
+#------------READER ----------------------
+rectGridReader = vtk.vtkRectilinearGridReader()
+rectGridReader.SetFileName("../data/jet4_0.500.vtk")
+rectGridReader.Update()
+#------------END READER ------------------
+
+#------------ FILTER: CALCULATE VECTOR MAGNITUDE ----------------------
+magnitudeCalcFilter = vtk.vtkArrayCalculator()
+magnitudeCalcFilter.SetInputConnection(rectGridReader.GetOutputPort())
+magnitudeCalcFilter.AddVectorArrayName('vectors')
+magnitudeCalcFilter.SetResultArrayName('magnitude')
+magnitudeCalcFilter.SetFunction("mag(vectors)") 
+magnitudeCalcFilter.Update()
+#------------END CALCULATE VECTOR MAGNITUDE ----------------------
+
+#------------FILTER: RECTILINEAR GRID TO IMAGE DATA-----------
+bounds = magnitudeCalcFilter.GetOutput().GetBounds()
+#print(bounds)
+dimensions = magnitudeCalcFilter.GetOutput().GetDimensions()
+#print(dimensions)
+origin = (bounds[0], bounds[2], bounds[4])
+spacing = ( (bounds[1]-bounds[0])/dimensions[0], 
+            (bounds[3]-bounds[2])/dimensions[1],
+            (bounds[5]-bounds[4])/dimensions[2])
+
+# we create image data
+imageData = vtk.vtkImageData()
+imageData.SetOrigin(origin)
+imageData.SetDimensions(dimensions)
+imageData.SetSpacing(spacing)
+
+probeFilter = vtk.vtkProbeFilter()
+probeFilter.SetInputData(imageData)
+probeFilter.SetSourceData(magnitudeCalcFilter.GetOutput())
+probeFilter.Update()
+
+imageData2 = probeFilter.GetImageDataOutput()
+#------------END RECTILINEAR GRID TO IMAGE DATA-----------
+
+##------------FILTER, MAPPER, AND ACTOR: VOLUME RENDERING -------------------
+# Create transfer mapping scalar value to opacity
+opacityTransferFunction = vtk.vtkPiecewiseFunction()
+opacityTransferFunction.AddPoint(0, 0.2)
+
+opacityTransferFunction.AddPoint(2, 0.7)
+opacityTransferFunction.AddPoint(5, 0.8)
+opacityTransferFunction.AddPoint(10, 0.9)
+
+opacityTransferFunction.AddPoint(13, 1.3)
+opacityTransferFunction.AddPoint(14, 2.1)
+
+
+
+# Create transfer mapping scalar value to color
+colorTransferFunction = vtk.vtkColorTransferFunction()
+colorTransferFunction.AddRGBPoint(0.0, 0.2, 0.0, 0.0)
+colorTransferFunction.AddRGBPoint(0.5, 1.7, 0.0, 0.0)
+colorTransferFunction.AddRGBPoint(2.0, 0.0, 0.0, 2.1)
+colorTransferFunction.AddRGBPoint(6.0, 0.0, 2.2, 0.0)
+colorTransferFunction.AddRGBPoint(14.0, 0.0, 3.1, 0.0)
+
+# The property describes how the data will look
+volumeProperty = vtk.vtkVolumeProperty()
+volumeProperty.SetColor(colorTransferFunction)
+volumeProperty.SetScalarOpacity(opacityTransferFunction)
+volumeProperty.ShadeOff()
+volumeProperty.SetInterpolationTypeToLinear()
+
+
+# The mapper / ray cast function know how to render the data
+#volumeMapper = vtk.vtkProjectedTetrahedraMapper()
+#volumeMapper = vtk.vtkUnstructuredGridVolumeZSweepMapper()
+volumeMapper = vtk.vtkGPUVolumeRayCastMapper()
+#volumeMapper = vtk.vtkUnstructuredGridVolumeRayCastMapper()
+volumeMapper.SetInputData(imageData2)
+
+# The volume holds the mapper and the property and
+# can be used to position/orient the volume
+volume = vtk.vtkVolume()
+volume.SetMapper(volumeMapper)
+volume.SetProperty(volumeProperty)
+
+##------------END VOLUME RENDERING ----------------------
+
+#---------RENDERER, RENDER WINDOW, AND INTERACTOR ----------
+renderer = vtk.vtkRenderer()
+renderer.SetBackground(0.5, 0.5, 0.5)
+renderer.AddVolume(volume)
+renderer.ResetCamera()
+
+renderWindow = vtk.vtkRenderWindow()
+renderWindow.AddRenderer(renderer)
+renderWindow.SetSize(500, 500)
+renderWindow.Render()
+
+iren = vtk.vtkRenderWindowInteractor()
+iren.SetRenderWindow(renderWindow)
+iren.Start()
+#---------END RENDERER, RENDER WINDOW, AND INTERACTOR -------
